0c0e0d7e8549b492c9fffd0d7f66e20ffb5d7ba9
Yograjsinh <yograjsarvaiya07@gmail.com>
2026-08-20 12:47:26 +0530

    Fix mobile navbar layout

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -28,10 +28,10 @@ export default function Header() {
             </Link>
           </div>
           <div
-            className="flex justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+            className="flex justify-between items-center w-full lg:w-auto lg:order-1"
             id="mobile-menu-2"
           >
-            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+            <ul className="flex mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
               <li>
                 <NavLink
                   to="/"
